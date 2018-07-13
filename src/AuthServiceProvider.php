a9bb7dfe672a2439cced43abc63350b76988bc2d
Hanson <h@hanc.cc>
2018-07-13 15:56:42 +0800

    add config publish

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -12,7 +12,7 @@ class AuthServiceProvider extends ServiceProvider
     public function register()
     {
         $this->publishes([__DIR__ . '/database/' => database_path()]);
-        $this->publishes([__DIR__ . '/database/' => database_path()]);
+        $this->publishes([__DIR__ . '/config/' => config_path()]);
     }
 
     public function boot()
